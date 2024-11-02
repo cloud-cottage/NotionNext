17b9e62975c72d25f01b7a3e91d0ea787fe5d537
cloud-cottage <cooncn@gmail.com>
2024-11-02 09:08:13 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -64,10 +64,8 @@ const BLOG = {
   // 字体CSS 例如 https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css
   FONT_URL: [
     'https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css',
-    'https://fonts.googleapis.com/css2?family=EB+Garamond'
-//    'https://fonts.googleapis.com/css?family=Bitter&display=swap',
-//    'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300&display=swap',
-//    'https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@300&display=swap'
+    'https://fonts.googleapis.com/css2?family=EB+Garamond',
+    'https://fonts.googleapis.com/css2?family=Roboto+SC:wght@300&display=swap'
   ],
   FONT_SANS: [
     '"Roboto"',
