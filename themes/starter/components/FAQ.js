aa29f9ff93c947b5f63103c32c5ad52b20e4ed46
cloud-cottage <cooncn@gmail.com>
2024-11-02 19:58:42 +0800

    Update FAQ.js

--- themes/starter/components/FAQ.js
+++ themes/starter/components/FAQ.js
@@ -25,7 +25,7 @@ export const FAQ = () => {
                 <span className='mb-2 block text-lg font-semibold text-primary'>
                   {siteConfig('STARTER_FAQ_TITLE')}
                 </span>
-                <h2 className='mb-3 text-3xl font-bold leading-[1.2] text-dark dark:text-white sm:text-4xl md:text-[40px]'>
+                <h2 className={`${siteConfig('FONT_STYLE_TITLE')} mb-3 text-3xl font-bold leading-[1.2] text-dark dark:text-white sm:text-4xl md:text-[40px]`}>
                   {siteConfig('STARTER_FAQ_TEXT_1')}
                 </h2>
                 <p className='mx-auto max-w-[485px] text-base text-body-color dark:text-dark-6'>
